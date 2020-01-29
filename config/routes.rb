99d2bc98752e746585c639184b897f93f3966dd8
dark_luminosity <engelbergsee@yahoo.com>
2020-01-29 21:52:05 +0800

    added license, code history and internal documentation

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
+# This is a course requirement for CS 192 Software Engineering II under the supervision of Asst. Prof. Ma. Rowena C. Solamo of the Department of Computer Science, College of Engineering, University of the Philippines, Diliman for the AY 2019-2020
+# - Engelberg See
+
+# Code History:
+# Engelberg See - 01/20/20 - Generated the routes for the application
+# Engelberg See - 01/22/20 - Modified to have a home page for the application instead of the default rails success page
+# Engelberg See - 01/29/20 - Added license, code history and Internal Documentation
+
+# File Creation Date: 01/20/20
+# Development Group: Engelberg See
+# Purpose: Essential for the application to have an index page where all actions can be made
+
 Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
